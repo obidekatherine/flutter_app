dc77ff106afe53353333b0c27a7bab956934e8f4
obide katherine <katherinedenniso@yahoo.com>
2021-05-16 19:33:55 +0100

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,47 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+void main() => runApp(SnackBarDemo());
+
+class SnackBarDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ZURI',
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          title: Text('ZURI'),
+          backgroundColor: Colors.blue,
+        ),
+        body: SnackBarPage(),
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class SnackBarPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        onPressed: () {
+          final snackBar = SnackBar(
+            backgroundColor: Colors.black87,
+            content: Text('Successful!'),
+            action: SnackBarAction(
+              label: 'Undo',
+              onPressed: () {
+                // Some code to undo the change.
+              },
+            ),
+          );
+          // Find the ScaffoldMessenger in the widget tree
+          // and use it to show a SnackBar.
+          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+        },
+        child: Text('Press To Join Zuri Training'),
+      ),
+    );
+  }
 }
